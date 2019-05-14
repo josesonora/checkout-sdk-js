4b416499161d0506624916adaa7d0120b6897045
Jose Sonora <jose.sonora@bigcommerce.com>
2019-05-14 17:27:57 -0500

    feat(payment): INT-1479 Linters

--- src/payment/create-payment-strategy-registry.ts
+++ src/payment/create-payment-strategy-registry.ts
@@ -31,6 +31,8 @@ import {
 import { ChasePayPaymentStrategy, ChasePayScriptLoader } from './strategies/chasepay';
 import { ConvergePaymentStrategy } from './strategies/converge';
 import { CreditCardPaymentStrategy } from './strategies/credit-card';
+import CyberSourcePaymentStrategy from './strategies/cybersource/cybersource-payment-strategy';
+import CyberSourceScriptLoader from './strategies/cybersource/cybersource-script-loader';
 import {
     createGooglePayPaymentProcessor,
     GooglePayBraintreeInitializer,
@@ -48,8 +50,6 @@ import { SagePayPaymentStrategy } from './strategies/sage-pay';
 import { SquarePaymentStrategy, SquareScriptLoader } from './strategies/square';
 import { WepayPaymentStrategy, WepayRiskClient } from './strategies/wepay';
 import { ZipPaymentStrategy, ZipScriptLoader } from './strategies/zip';
-import CyberSourcePaymentStrategy from "./strategies/cybersource/cybersource-payment-strategy";
-import CyberSourceScriptLoader from "./strategies/cybersource/cybersource-script-loader";
 
 export default function createPaymentStrategyRegistry(
     store: CheckoutStore,
