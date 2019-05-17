2da24380331f8392a8677f21dfa25348d94ec726
Jose Sonora <jose.sonora@bigcommerce.com>
2019-05-17 16:39:57 -0500

    feat(payment): INT-1479 Deploy

--- dist/checkout-sdk.d.ts
+++ dist/checkout-sdk.d.ts
@@ -2128,6 +2128,7 @@ declare interface CreditCardInstrument {
     ccCvv?: string;
     shouldSaveInstrument?: boolean;
     extraData?: any;
+    threeDSecure?: ThreeDSecure;
 }
 
 declare interface Currency {
@@ -3168,6 +3169,17 @@ declare interface Tax {
 declare interface TextInputStyles extends InputStyles {
     placeholder?: InlineElementStyles;
 }
+
+declare type ThreeDSecure = Partial<{
+    version: string;
+    status: string;
+    vendor: string;
+    cavv: string;
+    eci: string;
+    xid: string;
+    token: string;
+    session: string;
+}>;
 
 declare interface TranslationData {
     [key: string]: string | number;
